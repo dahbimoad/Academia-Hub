24636ed6ad652fe79dce9be417701fdc47099cd9
dahbimoad <dahbimoad1@gmail.com>
2025-01-02 01:39:10 +0100

    dev pulled

--- src/main/java/com/academiahub/schoolmanagement/utils/DatabaseConnection.java
+++ src/main/java/com/academiahub/schoolmanagement/utils/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/ecole";
+    private static final String URL = "jdbc:postgresql://localhost:5432/school_management";
     private static final String USER = "postgres";  // Replace with your PostgreSQL username
-    private static final String PASSWORD = "Imad2002";  // Replace with your PostgreSQL password
+    private static final String PASSWORD = "mouad1233";  // Replace with your PostgreSQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
